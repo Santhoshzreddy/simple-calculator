99aec0844665487191b46b94bd31fe92d25d6649
Santhoshzreddy <118095329+Santhoshzreddy@users.noreply.github.com>
2022-11-14 20:07:59 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,14 @@
+a=int(input("enter 1st num:   "))
+oper=input("enter one operator( + , - , * , / )")
+b=int(input("enter 2nd num:   "))
+if (oper == '+'):
+    print(a + b)
+elif (oper == '-'):
+    print(a - b)
+elif (oper == '*'):
+    print(a * b)
+elif (oper == '/'):
+    print(a / b)
+
+print("thank you")
+input("press enter to close")
